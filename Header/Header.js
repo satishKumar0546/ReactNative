bc6438a6255ac56a02cc2c3b6fe796daedc11156
Satish <satishkumar0546@gmail.com>
2018-08-07 20:37:55 +0530

    HelloWorld Program for React Native

---
+++ Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Componet {
+	render() {
+		return(
+			<header className="header">
+				<div className="headerContainer">Mobile Security</div>
+			</header>
+		);
+	}
+}
+
+export default Header;
