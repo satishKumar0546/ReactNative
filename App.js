6d3b93ebd582eaca3f7d5da1a4fea8d089019fd3
Satish <satishkumar0546@gmail.com>
2018-08-08 20:18:06 +0530

    Update for Picker

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput, Picker } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
 class HomeScreen extends React.Component {
+
+
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{borderColor:'black', flex: 1, alignItems: 'center'}}>
         <Text>Home Screen</Text>
+        <TextInput style={{height: 40}} className = "inputbox" />
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
@@ -17,10 +21,24 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        language:'js'
+      };
+    }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Picker
+  selectedValue={this.state.language}
+  style={{ height: 50, width: 100 }}
+  onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+  <Picker.Item label="Java" value="java" />
+  <Picker.Item label="JavaScript" value="js" />
+</Picker>
+
       </View>
     );
   }
